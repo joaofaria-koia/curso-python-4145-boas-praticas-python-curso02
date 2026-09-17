77e28ba7f555987c14a6d16ac1811a2ea6d21328
Joao Faria <jfaria@iaasbr.com>
2026-09-17 16:00:17 -0300

    Projeto boas práticas Python: Curso 2 (metade já feito)

---
+++ main.py
@@ -0,0 +1,24 @@
+from cliente import Cliente
+from item import Item
+from pedido.pedido_retirada import PedidoRetirada
+from pedido.pedido_delivery import PedidoDelivery
+from pagamento.pagamento_cartao import PagamentoCartao
+from pagamento.pagamento_pix import PagamentoPIX
+from pagamento.pagamento_factory import PagamentoFactory
+
+cliente = Cliente("João", "Rua Migrantes")
+item_um = Item("Pizza", 30.0)
+item_dois = Item("Refrigerante", 5.0)
+itens = [item_um, item_dois]
+
+taxa_entrega = 10.0
+pedido_retirada = PedidoRetirada(cliente, itens)
+pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
+
+valor_pedido = pedido_delivery.calcular_total()
+# pagamento_cartao = PagamentoCartao().processar(valor_pedido)
+# pagamento_pix = PagamentoPIX()
+# pagamento_pix.processar(valor_pedido)
+tipo_pagamento = "cartao"
+pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento)
+pagamento.processar(valor_pedido)
